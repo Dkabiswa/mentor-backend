67866fc0c40da1c36cb00dd89b08d0ac678ea88a
Kabiswa Davis <kabiswad@gmail.com>
2020-08-13 11:23:53 +0200

    development set up

---
+++ src/index.js
@@ -0,0 +1,27 @@
+/* eslint-disable no-console */
+import '@babel/polyfill';
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import fileupload from 'express-fileupload';
+// import routes from './routes';
+
+const app = express();
+
+app.enable('trust proxy');
+app.use(cors());
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use(
+  fileupload({
+    useTempFiles: true
+  })
+);
+
+// app.use('/', routes);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
